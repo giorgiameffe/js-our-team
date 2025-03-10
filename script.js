807508af921bf5e487619b78b80e32397ef164f2
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-10 18:26:34 +0100

    feat: complete exercise and add imgs

--- script.js
+++ script.js
@@ -38,3 +38,47 @@ const teamMembers = [
 ];
 
 console.log(teamMembers);
+
+// Elemento html di riferimento
+
+/* 
+<div class="col">
+  <div class="member-photo">
+    <img src="img/female1.png" alt="Laura Rossi photo">
+  </div>
+  <div class="member-info">
+    <h3 class="name">Laura Rossi</h3>
+    <div class="role">Front-end Developer</div>
+    <a href="#">laurarossi@team.com</a>
+  </div>
+</div> */
+
+const parentElement = document.querySelector('.row');
+
+let memberElement = '';
+
+for (let i = 0; i < teamMembers.length; i++) {
+
+  const currentElement = teamMembers[i];
+  console.log(currentElement);
+  memberElement += createHTMLElement(currentElement);
+}
+
+console.log(memberElement);
+
+parentElement.innerHTML = memberElement;
+
+function createHTMLElement (obj) {
+
+  return `<div class="col">
+  <div class="member-photo">
+    <img src="${obj.img}" alt="${obj.name}">
+  </div>
+  <div class="member-info">
+    <h3 class="name">${obj.name}</h3>
+    <div class="role">${obj.role}</div>
+    <a href="#">${obj.email}</a>
+  </div>
+</div>`
+
+}
